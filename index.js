3a1e28d1c8cca9483600a4d7d555e24e912a7a55
Wycliffe <wycliffekiprono4@gmail.com>
2023-03-16 14:22:42 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+
+function mondayWork (activity = "go to the office") {
+    return (`This Monday, I will ${activity}.`);
+}
+
+saturdayFun();
+saturdayFun("work from home");
+
+function wrapAdjective (string = "*") {
+    return function(parameter = "special") {
+        return `You are ${string}${parameter}${string}!`;
+    }
+
+}
